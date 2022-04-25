0c83eb3b8388a48dd3bc6dc39ed2f1b13bba62cc
shrivalli04 <shrivalli04@gmail.com>
2022-04-25 10:13:13 +0530

    Values controller added

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        //get method added
+        public ActionResult<string> GetValue()
+        {
+            return Ok("First");
+        }
+    }
+}
